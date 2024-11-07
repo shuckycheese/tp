bcc11969a259bcd666396477aa2824394526dd8a
shuckycheese <darrylshuck@yahoo.com.sg>
2024-11-07 15:53:25 +0800

    Add regex

--- src/main/java/seedu/address/model/person/Notes.java
+++ src/main/java/seedu/address/model/person/Notes.java
@@ -1,10 +1,15 @@
 package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 /**
  * Represents a Patient's notes in WardWatch.
  * Guarantees: immutable; is always valid
  */
 public class Notes {
+    public static final String MESSAGE_CONSTRAINTS =
+            "It must have at least 1 alphanumeric character and has a character limit of 80.";
+    public static final String VALIDATION_REGEX = "^(?=.*[A-Za-z0-9]).{1,80}$";
     public final String value;
 
     /**
@@ -14,8 +19,14 @@ public class Notes {
      */
     public Notes(String patientNotes) {
         requireNonNull(patientNotes);
+        checkArgument(isValidNote(patientNotes), MESSAGE_CONSTRAINTS);
         value = patientNotes;
     }
+
+    public static boolean isValidNote(String test) {
+        return test.matches(VALIDATION_REGEX) || test.isEmpty();
+    }
+
     @Override
     public String toString() {
         return value;
